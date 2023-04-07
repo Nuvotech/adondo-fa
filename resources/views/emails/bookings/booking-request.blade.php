8eb3c6dc8893932eb05a5f0ff03a11bdf4c525cf
Gires <91569072+girests@users.noreply.github.com>
2023-04-07 12:03:40 +0200

    confirmation email update

--- resources/views/emails/bookings/booking-request.blade.php
+++ resources/views/emails/bookings/booking-request.blade.php
@@ -1,26 +1,20 @@
 <x-mail::message>
-# Booking for {{ $booking_data['roomName'] }} at  {{ $booking_data['roomArea'] }}
+# Booking for {{ $booking_data['roomName'] }} <br /><br/>
 
 
 
-  **Customer Name:** {{ $booking_data['customerName'] }}; <br />
-  **Customer Email:** {{ $booking_data['customerEmail'] }}; <br />
-  **Customer Phone:** {{ $booking_data['customerPhone'] }}; <br />
-  **Customer Check-in:** {{ $booking_data['customerCheckInDate'] }}; <br />
-  **Customer Check-out:** {{ $booking_data['customerCheckOutDate'] }}; <br />
-  **Room Price:** R{{ $booking_data['roomPrice'] }} per night; <br />
+  **Customer Name:**  {{ $booking_data['customerName'] }}; <br />
+  **Customer Email:**  {{ $booking_data['customerEmail'] }}; <br />
+  **Customer Phone:**  {{ $booking_data['customerPhone'] }}; <br />
+  **Customer Check-in:**  {{ $booking_data['customerCheckInDate'] }}; <br />
+  **Customer Check-out:**  {{ $booking_data['customerCheckOutDate'] }}; <br />
+  **Room Price:**  R{{ $booking_data['roomPrice'] }} per night; <br />
 
 
-    {{ $booking_data['customerMessage'] }};
+{{ $booking_data['customerMessage'] }};
 
 
 <br />
 <br />
 
-{{-- <x-mail::button :url="''">
-Button Text
-</x-mail::button> --}}
-
-Thanks,<br>
-{{ config('app.name') }}
 </x-mail::message>
