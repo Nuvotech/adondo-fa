04d0986a502aa050cd58cebe720fa281605f02ea
Mutombo <91569072+girests@users.noreply.github.com>
2024-01-23 04:57:26 +0200

    Address change

---
+++ public/build/assets/Welcome.fd4ba8fe.js
@@ -0,0 +1,1 @@
+import{a as e,F as o,j as r,H as a}from"./app.93df8acb.js";import{H as m,F as t}from"./Footer.9e7cc7f1.js";import s from"./Home.d9786339.js";import"./open-closed.9f8dd38c.js";import"./index.esm.5b4f3d33.js";function x(i){return e(o,{children:r("div",{className:"max-w-[1440px] mx-auto bg-white",children:[e(a,{title:"Welcome"}),e(m,{}),e(s,{}),e(t,{})]})})}export{x as default};
